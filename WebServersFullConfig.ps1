8ef61dabddeabad336851c0e94eb1e6e9806454b
Istvan Cebrian <istvan.cebrian@gmail.com>
2017-10-15 21:56:32 +0100

    new

---
+++ WebServersFullConfig.ps1
@@ -0,0 +1,66 @@
+Configuration WebServersConfig
+{
+    Import-DscResource -Module cChoco
+	Node ("localhost")
+	{
+		#Install the IIS Role
+		WindowsFeature IIS
+		{
+			Ensure = "Present"
+			Name = "Web-Server"
+        }
+        
+        #Install ASP.NET 4.5
+		WindowsFeature ASP45
+		{
+			Ensure = "Present"
+			Name = "Web-Asp-Net45"
+		}
+
+		#Install ASP.NET 3.5
+		WindowsFeature ASP35
+		{
+			Ensure = "Present"
+			Name = "Web-Asp-Net"
+        }
+        
+        WindowsFeature TelnetClient 
+        {
+            Ensure = 'Present'
+            Name = 'Telnet-Client'
+        }
+
+        Registry RegistryExample
+        {
+            Ensure      = "Present"  # You can also set Ensure to "Absent"
+            Key         = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\Current Version\Policies\System"
+            ValueName   = "legalnotiecaption"
+            ValueData   = "Notice to All Users (Authorized or Unauthorized)."
+        }
+
+        Registry RegistryExample
+        {
+            Ensure      = "Present"  # You can also set Ensure to "Absent"
+            Key         = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\Current Version\Policies\System"
+            ValueName   = "legalnoticetext"
+            ValueData   = "This computer system is for authorized use only. Users have no explicit or implicit expectation of privacy."
+        }
+
+        LocalConfigurationManager
+        {
+            DebugMode = 'ForceModuleImport'
+        }
+
+        cChocoInstaller installChoco
+        {
+            InstallDir = "c:\choco"
+        }
+
+        cChocoPackageInstaller installChrome
+        {
+            Name = "googlechrome"
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+
+	}
+}
